efda71f2461fd0581db212d75b130c0b14bca702
Aly <el7amrawy@outlook.com>
2023-03-26 13:26:39 +0200

    add activeTab state

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -5,32 +5,50 @@ import {
   faHeart,
   faCirclePlay,
 } from "@fortawesome/free-solid-svg-icons";
+import { useState, SyntheticEvent } from "react";
 
 const SideBar = () => {
+  const [activeTab, setActiveTab] = useState("home");
+
+  const clickHandler = (ev: SyntheticEvent) => {
+    const activeTab = document.querySelector("#sidebar > div.active-tab");
+    const target = ev.target as HTMLElement;
+    let tabName = "";
+    activeTab?.classList.remove("active-tab");
+    if (target.tagName === "A") {
+      target.parentElement?.classList.add("active-tab");
+      tabName = target.textContent as unknown as string;
+    } else {
+      target.classList.add("active-tab");
+      tabName = target.firstChild?.textContent as unknown as string;
+    }
+    setActiveTab(tabName);
+  };
+
   return (
     <aside id="sidebar">
-      <div className="active-tab">
+      <div className="active-tab" onClick={clickHandler}>
         <a>
           <FontAwesomeIcon icon={faBarsStaggered} />
-          Home
+          home
         </a>
       </div>
-      <div>
+      <div onClick={clickHandler}>
         <a>
           <FontAwesomeIcon icon={faMagnifyingGlass} />
-          Search
+          search
         </a>
       </div>
-      <div>
+      <div onClick={clickHandler}>
         <a>
           <FontAwesomeIcon icon={faHeart} />
-          Favourites
+          favourites
         </a>
       </div>
-      <div>
+      <div onClick={clickHandler}>
         <a>
           <FontAwesomeIcon icon={faCirclePlay} />
-          Playlists
+          playlists
         </a>
       </div>
     </aside>
