d9a9653de5468c4f2a791c1cae4cb68d783e7f26
Aly <el7amrawy@outlook.com>
2023-03-28 00:35:47 +0200

    add favourite song

--- src/components/Song.tsx
+++ src/components/Song.tsx
@@ -1,3 +1,7 @@
+import { faHeart } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useEffect, useRef, useState } from "react";
+
 type Img = {
   background: string;
   coverart: string;
@@ -15,8 +19,26 @@ type SongProps = {
 
 const Song = (props: SongProps) => {
   const { song } = props;
+  /* ------------- states ------------------- */
+  const [favourite, setFavourite] = useState(false);
+  /* -------------------------------- */
+  const ref = useRef(null);
+  /* ------------- effects ------------------- */
+  useEffect(() => {
+    if (favourite) {
+      (ref.current as unknown as HTMLElement).classList.remove("hidden");
+    } else {
+      (ref.current as unknown as HTMLElement).classList.add("hidden");
+    }
+  }, [favourite]);
   return (
     <div className="song">
+      <FontAwesomeIcon
+        icon={faHeart}
+        onClick={() => setFavourite(!favourite)}
+        style={{ color: favourite ? "red" : "var(--primary)" }}
+        ref={ref}
+      />
       <div>
         <img src={song.images.coverart} />
       </div>
